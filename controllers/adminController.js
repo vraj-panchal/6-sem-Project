b9d52319d409b71c2d0927cec3fe6081081613ae
Vraj Panchal <vrajpanchal110106@gmail.com>
2026-02-03 20:46:13 +0530

    sameSite: none

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -74,7 +74,7 @@ export const registerAdmin = async (req, res) => {
     res.cookie("token_ax", token, {
       httpOnly: true,
       maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
-      sameSite: "strict",
+      sameSite: "none",
       secure: process.env.NODE_ENV === "production",
     });
 
