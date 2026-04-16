06dd187d2ad3d941031860c25ae6b9f922a9ab6f
Vraj Panchal <vrajpanchal110106@gmail.com>
2026-04-16 20:07:50 +0530

    corect

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const allowedOrigins = [
     'http://127.0.0.1:3000',
     'http://127.0.0.1:5173',
     'https://six-sem-project.onrender.com/',
+    'https://project-tau-dusky.vercel.app',
     process.env.FRONTEND_URL,
 ].filter(Boolean);
 
